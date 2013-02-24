7f72a8483705b32ecdca3d4daca432690d43a14a
Matt Travi <programmer@travi.org>
2013-02-24 17:04:19 -0600

    moved gruntfile to root

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/*global module*/
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-bower');
+
+    // Project configuration.
+    grunt.initConfig({
+        lint: {
+            all: ['grunt.js', 'js/**/*.js']
+        },
+
+        jslint: {
+            files: ['grunt.js', 'js/**/*.js'],
+            directives: {
+                browser: true,
+                predef: [
+                    '$',
+                    'jQuery',
+                    'Modernizr',
+                    'travi'
+                ]
+            },
+            options: {
+                errorsOnly: true
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', 'jslint');
+
+};
